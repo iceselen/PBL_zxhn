9ba630a855538fbeebb03e6855fb1df8704bef9a
iceselen <iceselene@outlook.com>
2023-09-04 18:31:50 +0800

    fix the namespace

--- src/main/java/com/zxhn/dao/CartMapper.java
+++ src/main/java/com/zxhn/dao/CartMapper.java
@@ -2,20 +2,23 @@ package com.zxhn.dao;
 
 import com.zxhn.pojo.Cart;
 import org.apache.ibatis.annotations.Param;
+//import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
+//购物车相关组件接口
 public interface CartMapper {
-    int deleteByPrimaryKey(Integer id);
-
+    //根据组件删除
+    int deleteByPrimaryKey(@Param("id") Integer id);
+    //直接插入
     int insert(Cart record);
-
+    //有选择的插入
     int insertSelective(Cart record);
-
+    //根据组件查询对象
     Cart selectByPrimaryKey(Integer id);
-
+    //有选择的根据组件更新
     int updateByPrimaryKeySelective(Cart record);
-
+    //根据组件更新
     int updateByPrimaryKey(Cart record);
 
     Cart selectCartByUserIdProductId(@Param("userId") Integer userId, @Param("productId")Integer productId);

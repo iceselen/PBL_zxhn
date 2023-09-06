4f52ec73c2aad6bc39e56fec60ef485d0028fc1a
iceselen <iceselene@outlook.com>
2023-09-06 13:12:08 +0800

    change string and tomact version

--- src/main/java/com/zxhn/dao/CartMapper.java
+++ src/main/java/com/zxhn/dao/CartMapper.java
@@ -8,17 +8,17 @@ import java.util.List;
 
 //购物车相关组件接口
 public interface CartMapper {
-    //根据组件删除
+
     int deleteByPrimaryKey(@Param("id") Integer id);
-    //直接插入
+
     int insert(Cart record);
-    //有选择的插入
+
     int insertSelective(Cart record);
-    //根据组件查询对象
+
     Cart selectByPrimaryKey(Integer id);
-    //有选择的根据组件更新
+
     int updateByPrimaryKeySelective(Cart record);
-    //根据组件更新
+
     int updateByPrimaryKey(Cart record);
 
     Cart selectCartByUserIdProductId(@Param("userId") Integer userId, @Param("productId")Integer productId);
